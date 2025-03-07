a573737053b60083209b284d453d0b9323d98466
AndreyZ15 <148409598+AndreyZ15@users.noreply.github.com>
2025-03-07 16:14:41 -0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB7Ta7T_dDgvx3r26IdKKt6e-7b0hOekSI",
-    authDomain: "examen1progra4-a63fb.firebaseapp.com",
-    projectId: "examen1progra4-a63fb",
-    storageBucket: "examen1progra4-a63fb.firebasestorage.app",
-    messagingSenderId: "264781726632",
-    appId: "1:264781726632:web:fdfc46fc1adfde77dbd39d"
+      apiKey: "TU_API_KEY",
+    authDomain: "TU_AUTH_DOMAIN",
+    projectId: "TU_PROJECT_ID",
+    storageBucket: "TU_STORAGE_BUCKET",
+    messagingSenderId: "TU_MESSAGING_SENDER_ID",
+    appId: "TU_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
